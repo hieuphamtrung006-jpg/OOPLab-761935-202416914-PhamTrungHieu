7a6e2124a24942461bd7dfaebb9f7c5816645bb4
Dung-tech <hieuphamtrung006@gmail.com>
2026-04-05 02:48:23 +0700

    .

---
+++ AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,58 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private int id;
+    
+    
+    private static int nbDigitalVideoDiscs = 0;
+
+ 
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        updateID();
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+        updateID();
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+        updateID();
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        updateID();
+    }
+
+   
+    private void updateID() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
+    public int getId() { return id; }
+
+  
+    public void setTitle(String title) { this.title = title; }
+}
